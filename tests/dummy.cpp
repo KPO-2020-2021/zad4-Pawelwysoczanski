c8c7bed4549cbccd6deb9217e5f66fb4def606dc
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-05-17 20:21:38 +0200

    Add files via upload

--- tests/dummy.cpp
+++ tests/dummy.cpp
@@ -1,11 +1,213 @@
-#include "doctest.h"
+#include "../tests/doctest/doctest.h"
 #include "example.h"
+#include "Macierz3x3.hh"
+#include "Wektor3D.hh"
+#include "Prostopadloscian.hh"
+#define DL_BOKU 50
+#define SZ_BOKU 60
+#define WYSOKOSC_BOKU 30
 
+#define SIZE 3
 // Tests that don't naturally fit in the headers/.cpp files directly
 // can be placed in a tests/*.cpp file. Integration tests are a good example.
 
-TEST_CASE("complicated integration tests could be here")
+/*Testy klasy Matrix*/
+TEST_CASE("Konstruktor prametryczny")
 {
-  Dummy d;
-  CHECK(d.doSomething() == true);
+    double m1[][SIZE] = {{3, 2, 4}, {1, 5, 6}};
+    double m2[][SIZE] = {{3, 5, 7}, {6, 7, 5}};
+
+    Macierz3x3 ma(m1);
+
+    CHECK(ma == Matrix(m1));
+    CHECK(ma != Matrix(m2));
 }
+TEST_CASE("Konstruktor bezparametryczny")
+{
+    double mat[][SIZE] = {{0, 0, 0},
+                          {0, 0, 0},
+                          {0, 0, 0}};
+    Macierz3x3 ma1;
+    Macierz3x3 ma2(mat);
+    CHECK_EQ(ma1, ma2);
+}
+TEST_CASE("Operator *,test 1")
+{
+    double w1[] = {19, 19, 32};
+    double w2[] = {1, 2, 4};
+    double m1[][SIZE] = {{3, 2, 3}, {1, 5, 2}, {2, 3, 6}};
+
+    Macierz3x3 ma1(m1);
+
+    Vector3D we1(w2);
+    Vector3D we2;
+    Vector3D we3(w1);
+
+    we2 = ma1 * we1;
+
+    CHECK_EQ(we2, we3);
+}
+TEST_CASE("Operator *,test 2")
+{
+    double w1[] = {-19, -19, -32};
+    double w2[] = {-1, -2, -4};
+    double m1[][SIZE] = {{3, 2, 3}, {1, 5, 2}, {2, 3, 6}};
+
+    Macierz3x3 ma1(m1);
+
+    Vector3D we1(w2);
+    Vector3D we2;
+    Vector3D we3(w1);
+
+    we2 = ma1 * we1;
+
+    CHECK_EQ(we2, we3);
+}
+
+/*Testy dla klasy Cuboid*/
+TEST_CASE("Konsturktor bezparametryczny")
+{
+    int dl = 0;
+    int sz = 0;
+    int wys = 0;
+    double we[] = {0, 0, 0};
+    Vector3D ve(we);
+    Cuboid cub2(ve, dl, sz, wys);
+    Cuboid cub1;
+    CHECK_EQ(cub1, cub2);
+}
+TEST_CASE("Konstruktor parametryczny")
+{
+    double w1[] = {3, 4, 6};
+    double w2[] = {8, 9, 7};
+
+    Vector3D we1(w1);
+    Vector3D we2(w2);
+
+    Cuboid cub1(we1, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+    Cuboid cub2(we2, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+
+    CHECK_EQ(cub1, cub1);
+}
+TEST_CASE("Translacja,test 1")
+{
+    double w1[] = {3, 4, 5};
+    double w2[] = {8, 9, 8};
+    double w3[] = {11, 13, 13};
+
+    Vector3D we1(w1);
+    Vector3D we2(w2);
+    Vector3D we3(w3);
+
+    Cuboid cub1(we1, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+    Cuboid cub2(we3, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+
+    std::cout << cub1 << std::endl;
+    std::cout << std::endl;
+    std::cout << cub2 << std::endl;
+    std::cout << std::endl;
+    cub1.translation(we2);
+    std::cout << cub1 << std::endl;
+    std::cout << std::endl;
+    CHECK_EQ(cub1, cub2);
+}
+TEST_CASE("Translacja,test 2")
+{
+    double w1[] = {3, 4, 5};
+    double w2[] = {-8, -9, -11};
+    double w3[] = {-5, -5, -6};
+
+    Vector3D we1(w1);
+    Vector3D we2(w2);
+    Vector3D we3(w3);
+
+    Cuboid cub1(we1, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+    Cuboid cub2(we3, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+
+    std::cout << cub1 << std::endl;
+    std::cout << std::endl;
+    std::cout << cub2 << std::endl;
+    std::cout << std::endl;
+    cub1.translation(we2);
+    std::cout << cub1 << std::endl;
+    std::cout << std::endl;
+
+    CHECK_EQ(cub1, cub2);
+}
+TEST_CASE("Translacja,test 3")
+{
+    double w1[] = {3, 4, 6};
+    double w2[] = {0, -9, -7};
+    double w3[] = {3, -5, -1};
+
+    Vector3D we1(w1);
+    Vector3D we2(w2);
+    Vector3D we3(w3);
+
+    Cuboid cub1(we1, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+    Cuboid cub2(we3, WYSOKOSC_BOKU, SZ_BOKU, DL_BOKU);
+
+    std::cout << cub1 << std::endl;
+    std::cout << std::endl;
+    std::cout << cub2 << std::endl;
+    std::cout << std::endl;
+    cub1.translation(we2);
+    std::cout << cub1 << std::endl;
+    std::cout << std::endl;
+    CHECK_EQ(cub1, cub2);
+}
+// TEST_CASE("Rotacja,test 1")
+// {
+//     double w1[] = {3, 4};
+//     double w2[] = {3, 4};
+
+//     Vector3D we1(w1);
+//     Vector3D we2(w2);
+
+//     Cuboid re1(we1, DL_KROTKI_BOK, DL_DLUGI_BOK);
+//     Cuboid re2(we2, DL_KROTKI_BOK, DL_DLUGI_BOK);
+
+//     Macierz3x3 ma;
+
+//     ma.Calculate(90);
+
+//     re1 = ma * re1;
+//     re2 = ma * re2;
+
+//     CHECK_EQ(re1, re2);
+// }
+// TEST_CASE("Rotacja,test 2")
+// {
+//     double w1[] = {3, 4};
+//     double w2[] = {3, 4};
+
+//     Vector3D we1(w1);
+//     Vector3D we2(w2);
+
+//     Cuboid re1(we1, DL_KROTKI_BOK, DL_DLUGI_BOK);
+//     Cuboid re2(we2, DL_KROTKI_BOK, DL_DLUGI_BOK);
+
+//     Macierz3x3 ma;
+
+//     ma.Calculate(-90);
+
+//     re1 = ma * re1;
+//     re2 = ma * re2;
+
+//     CHECK_EQ(re1, re2);
+// }
+// TEST_CASE("Długość boków")
+// {
+//     double w1[] = {3, 4, 5};
+
+//     Vector3D we1(w1);
+
+//     Cuboid re1(we1, DL_KROTKI_BOK, DL_DLUGI_BOK);
+
+//     re1.length();
+
+//     CHECK_EQ(re1, re1);
+// }
+
+// This is all that is needed to compile a test-runner executable.
+// More tests can be added here, or in a new tests/*.cpp file.
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,238 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-
+#include "../tests/doctest/doctest.h"
+#include "dummy.cpp"
 // This is all that is needed to compile a test-runner executable.
 // More tests can be added here, or in a new tests/*.cpp file.
+
+#include "Wektor3D.hh"
+#include "Macierz3x3.hh"
+
+TEST_CASE("Konstruktor bez parametryczny")
+{
+    double we[] = {0, 0, 0};
+
+    Vector3D ve1;
+    Vector3D ve2(we);
+    CHECK_EQ(ve1, ve2);
+}
+TEST_CASE("Konstruktor parametryczny")
+{
+    std::stringstream in;
+
+    double we[] = {10, 20, 30};
+
+    Vector3D ve2;
+
+    in << "10 20 30" << std::endl;
+    in >> ve2;
+    Vector3D ve1(we);
+    CHECK_EQ(ve1, ve2);
+}
+TEST_CASE("Operator +, TEST 1")
+{
+    double we1[] = {10, 20, 30};
+    double we2[] = {5, 10, 15};
+    double we3[] = {15, 30, 45};
+
+    Vector3D ve1(we1);
+    Vector3D ve2(we2);
+    Vector3D ve3(we3);
+
+    CHECK_EQ(ve1 + ve2, ve3);
+}
+TEST_CASE("Operator +, TEST 2")
+{
+    double we1[] = {10.001, 20.001, 30.001};
+    double we2[] = {5.999, 10.999, 15.999};
+    double we3[] = {16, 31, 46};
+
+    Vector3D ve1(we1);
+    Vector3D ve2(we2);
+    Vector3D ve3(we3);
+
+    CHECK_EQ(ve1 + ve2, ve3);
+}
+TEST_CASE("Operator -, TEST 1")
+{
+    double we1[] = {10, 20, 30};
+    double we2[] = {5, 10, 15};
+    double we3[] = {5, 10, 15};
+
+    Vector3D ve1(we1);
+    Vector3D ve2(we2);
+    Vector3D ve3(we3);
+
+    CHECK_EQ(ve1 - ve2, ve3);
+}
+TEST_CASE("Operator -, TEST 2")
+{
+    double we1[] = {10.001, 20.001, 30.001};
+    double we2[] = {5.999, 10.999, 15.999};
+    double we3[] = {4.002, 9.002, 14.002};
+
+    Vector3D ve1(we1);
+    Vector3D ve2(we2);
+    Vector3D ve3(we3);
+
+    CHECK_EQ(ve1 - ve2, ve3);
+}
+TEST_CASE("Operator wczytywania >>,test 1")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {4, 1, 5};
+    Vector3D we2(w);
+
+    in << "4 1 5" << std::endl;
+    in >> we1;
+
+    CHECK_EQ(we1, we2);
+}
+
+TEST_CASE("Operator wczytywania >>,test 2")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {0, 1, 2};
+    Vector3D we2(w);
+
+    in << "0 1 2" << std::endl;
+    in >> we1;
+
+    CHECK_EQ(we1, we2);
+}
+TEST_CASE("Operator wczytywania >>,test 3")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {0, 0, 0};
+    Vector3D we2(w);
+
+    in << "0 0 0" << std::endl;
+    in >> we1;
+
+    CHECK_EQ(we1, we2);
+}
+TEST_CASE("Operator wczytywania >>,test 4")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {0, 0, 0};
+    Vector3D we2(w);
+
+    in << "p 1 4" << std::endl;
+    in >> we1;
+    CHECK_EQ(we1, we2);
+}
+TEST_CASE("Operator wczytywania >>,test 5")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {0, 0, 0};
+    Vector3D we2(w);
+
+    in << "4 d 5" << std::endl;
+    in >> we1;
+    CHECK_EQ(we1, we2);
+}
+TEST_CASE("Operator wczytywania >>,test 6")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {4.888, -3.444, 5.666};
+    Vector3D we2(w);
+
+    in << "4.888 -3.444 5.666" << std::endl;
+    in >> we1;
+
+    CHECK_EQ(we1, we2);
+}
+TEST_CASE("Operator wczytywania >>,test 7")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {-4, 3, 5};
+    Vector3D we2(w);
+
+    in << "-4 3 5" << std::endl;
+    in >> we1;
+    CHECK_EQ(we1, we2);
+}
+TEST_CASE("Operator wczytywania >>,test 8")
+{
+    std::stringstream in;
+
+    Vector3D we1;
+    double w[] = {0, 0, 0};
+    Vector3D we2(w);
+
+    in << "4 1 e" << std::endl;
+    in >> we1;
+    CHECK_EQ(we1, we2);
+}
+
+TEST_CASE("Operator wypisywania <<,test 1")
+{
+    std::stringstream out;
+
+    double w[] = {-4, 3, 6};
+
+    Vector3D we1;
+    Vector3D we2(w);
+
+    out << we2;
+
+    CHECK_EQ("-4 3 6", out.str());
+}
+TEST_CASE("Operator wypisywania <<,test 2")
+{
+    std::stringstream out;
+
+    double w[] = {-4.888, 3.444, 5.666};
+
+    Vector3D we1;
+    Vector3D we2(w);
+
+    out << we2;
+
+    CHECK_EQ("-4.888 3.444 5.666", out.str());
+}
+
+TEST_CASE("Operator []")
+{
+    double w1[] = {6, 7, 5};
+    double w2[] = {9, 4, 5};
+    double w3[] = {34, 78, 560};
+
+    Vector3D we1(w1);
+    Vector3D we2(w2);
+    Vector3D we3(w3);
+
+    double x1, x2, x3, y1, y2, y3, z1, z2, z3;
+    x1 = 6;
+    x2 = 9;
+    x3 = 34;
+    y1 = 7;
+    y2 = 4;
+    y3 = 78;
+    z1 = 5;
+    z2 = 5;
+    z3 = 560;
+
+    CHECK(x1 == w1[0]);
+    CHECK(y1 == w1[1]);
+    CHECK(z1 == w1[2]);
+    CHECK(x2 == w2[0]);
+    CHECK(y2 == w2[1]);
+    CHECK(z2 == w2[2]);
+    CHECK(x3 == w3[0]);
+    CHECK(y3 == w3[1]);
+    CHECK(z3 == w3[2]);
+}
